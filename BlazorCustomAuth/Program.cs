75ccbcc117018fd449538f51de21abed008f7e56
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-08-31 23:14:31 +0200

    17:30

--- BlazorCustomAuth/Program.cs
+++ BlazorCustomAuth/Program.cs
@@ -1,12 +1,28 @@
 using BlazorCustomAuth.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(cs));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedEmail = false;
+});
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
